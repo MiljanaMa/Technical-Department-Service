5d99961f3ec07890592166928a9383bec46d9fef
milena <milenamarkovickg@gmail.com>
2024-06-14 17:39:08 +0200

    feat: add side navbar

---
+++ Technical-Department-front/src/app/utils/material/material.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatInput, MatInputModule} from "@angular/material/input"
+import {MatSelect, MatSelectModule} from "@angular/material/select"
+import {MatAutocomplete, MatAutocompleteModule} from "@angular/material/autocomplete"
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar"
+import {MatMenu, MatMenuModule} from "@angular/material/menu"
+import {MatIcon, MatIconModule} from "@angular/material/icon"
+import {MatButton, MatButtonModule} from "@angular/material/button"
+import {MatBadge, MatBadgeModule} from "@angular/material/badge"
+import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule} from "@angular/material/sidenav"
+import {MatList, MatListItem, MatListModule, MatNavList} from "@angular/material/list"
+import {MatCard, MatCardModule} from "@angular/material/card"
+import {MatSlider, MatSliderModule} from "@angular/material/slider"
+import {MatTable, MatTableModule} from "@angular/material/table"
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator"
+import {MatSort, MatSortModule} from "@angular/material/sort"
+import {MatDatepicker, MatDatepickerModule} from "@angular/material/datepicker"
+import {MatNativeDateModule} from "@angular/material/core"
+import {MatRadioButton, MatRadioModule} from "@angular/material/radio"
+import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox"
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDividerModule
+  ],
+  exports: [
+    MatInput,
+    MatSelect,
+    MatAutocomplete,
+    MatToolbar,
+    MatMenu,
+    MatIcon,
+    MatButton,
+    MatBadge,
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatList,
+    MatCard,
+    MatSlider,
+    MatTable,
+    MatPaginator,
+    MatSort,
+    MatDatepicker,
+    MatRadioButton,
+    MatCheckbox,
+    MatDrawer,
+    MatDrawerContainer,
+    MatDrawerContent,
+    MatListItem,
+    MatNavList,
+    MatDivider
+  ]
+})
+export class MaterialModule { }
