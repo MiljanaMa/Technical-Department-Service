fbf49d0ed1c068471eb4835515169ca73a0ee26d
MiljanaMa <miljana.marjanovic9@gmail.com>
2024-06-15 20:05:24 +0200

    feat: new menu view

--- Technical-Department-front/src/app/app-routing.module.ts
+++ Technical-Department-front/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './layout/home/home.component';
 import { MenuComponent } from './kitchen/menu/menu.component';
 import { MealsComponent } from './kitchen/meals/meals.component';
 import { IngredientsComponent } from './kitchen/ingredients/ingredients.component';
+import { DailyMenuComponent } from './kitchen/daily-menu/daily-menu.component';
 
 const routes: Routes = [
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'menu', component: MenuComponent},
   {path: 'meals', component: MealsComponent},
   {path: 'ingredients', component: IngredientsComponent},
+  {path: 'test', component: DailyMenuComponent},
 ];
 
 @NgModule({
--- Technical-Department-front/src/app/kitchen/kitchen.module.ts
+++ Technical-Department-front/src/app/kitchen/kitchen.module.ts
@@ -5,6 +5,17 @@ import { IngredientsComponent } from './ingredients/ingredients.component';
 import { MealsComponent } from './meals/meals.component';
 import { MaterialModule } from '../utils/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DailyMenuComponent } from './daily-menu/daily-menu.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MealChangeModalComponent } from './meal-change-modal/meal-change-modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 
@@ -12,17 +23,39 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     MenuComponent,
     IngredientsComponent,
-    MealsComponent
+    MealsComponent,
+    DailyMenuComponent,
+    MealChangeModalComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDialogModule 
   ],
   exports: [
     MenuComponent,
     IngredientsComponent,
-    MealsComponent
+    MealsComponent,
+    CommonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDialogModule,
   ]
 })
 export class KitchenModule { }
