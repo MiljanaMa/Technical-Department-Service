98b0856f0384e9ede4a6b1d9b2217b458f8cf17d
MiljanaMa <miljana.marjanovic9@gmail.com>
2024-08-02 13:10:30 +0200

    feat: add all ingredients from excel

--- insert-script-new.sql
+++ insert-script-new.sql
@@ -1,100 +1,126 @@
+INSERT INTO kitchen."Ingredients" VALUES (1, 'Mrkva', 41, 0.9, 9.6, 0.2, 4.7, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (2, 'Beli luk', 35, 0.9, 7.7, 0.1, 0, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (3, 'Crni luk', 38, 0.8, 8.6, 0.1, 0, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (4, 'Pasulj šareni', 127, 8.9, 22.8, 0.5, 0.6, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (5, 'Slanina', 548, 36, 1.4, 43, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (6, 'Brašno tip 400-pšenično', 340, 13, 72, 2.5, 0.4, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (7, 'Paprika mljevena slatka', 282, 14, 54, 13, 10, 5, 11);
+INSERT INTO kitchen."Ingredients" VALUES (9, 'Jaja', 143, 13, 0, 9.5, 0, 3, 12);
+INSERT INTO kitchen."Ingredients" VALUES (10, 'Riba oslic', 78, 18, 0, 0.9, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (11, 'Krompir', 22, 1, 4.7, 0, 0, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (13, 'Jogurt', 63, 5.3, 7, 1.6, 7, 3, 13);
+INSERT INTO kitchen."Ingredients" VALUES (14, 'Sir svjež kravlji', 350, 6.2, 5.5, 34, 3.8, 3, 11);
+INSERT INTO kitchen."Ingredients" VALUES (15, 'Kore za pitu', 299, 7.1, 53, 6, 0.2, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (16, 'Prašak za pecivo', 0, 0, 0, 0, 0, 5, 11);
+INSERT INTO kitchen."Ingredients" VALUES (17, 'Svinjetina', 263, 17, 0, 21, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (18, 'Sojine ljuspice', 364, 9.1, 82, 0, 9.1, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (19, 'Makaroni sa jajima', 367, 13, 75, 1, 0, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (20, 'Mlijeko svježe', 60, 3.3, 4.7, 3.2, 4.7, 13);
+INSERT INTO kitchen."Ingredients" VALUES (21, 'Piletina', 440, 9.6, 0.8, 44, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (22, 'Pirinač dugo zrno', 360, 6.6, 79, 0.6, 0, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (23, 'Kupus', 25, 1.3, 5.8, 0.1, 3.2, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (24, 'Integralni pirinač', 362, 7.5, 76, 2.7, 0, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (25, 'Junetina', 176, 20, 0, 10, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (26, 'Žuta paprika svježa', 27, 1.0, 6.0, 0.2, 2.4, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (27, 'Paradajz', 27, 0.8, 5.5, 0.6, 0, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (28, 'Boranija', 31, 1.8, 7, 0.2, 3.3, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (29, 'Brokoli', 34, 2.8, 6.6, 0.4, 1.7, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (30, 'Paradajz pire', 24, 1.2, 5.3, 0.3, 3.6, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (31, 'Margarin', 533, 0.2, 0.9, 60, 0, 3, 11);
+INSERT INTO kitchen."Ingredients" VALUES (32, 'Kiseli krastavac', 11, 0.3, 2.3, 0.2, 1.1, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (33, 'Patlidzan', 25, 1, 5.9, 0.2, 3.5, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (34, 'Djuvec', 56, 2.7, 9.6, 1, 2, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (35, 'Špinat', 23, 2.9, 3.6, 0.4, 0.4, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (36, 'Grašak', 81, 5.4, 14, 0.4, 5.7, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (37, 'Kečap blagi', 101, 1, 27, 0.1, 21, 5, 13);
+INSERT INTO kitchen."Ingredients" VALUES (38, 'Makaroni bez jaja', 347, 13, 64, 2.5, 3.5, 2, 11);
+INSERT INTO kitchen."Ingredients" VALUES (39, 'Karfiol', 25, 1.9, 5, 0.3, 1.9, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (40, 'Tikvice', 17, 1.2, 3.2, 0.3, 2.5, 1, 11);
+INSERT INTO kitchen."Ingredients" VALUES (41, 'Pilece grudi', 110, 23, 0, 1.3, 0, 4, 11);
+INSERT INTO kitchen."Ingredients" VALUES (8, 'Ulje jestivo', 812, 0.5, 0, 91.6, 0, 0, 13);
+INSERT INTO kitchen."Ingredients" VALUES (12, 'Sirce alkoholno', 6, 1.2, 0, 0, 0, 5, 13);
+
 INSERT INTO kitchen."Ingredients"(
 	"Id", "Name", "Calories", "Proteins", "Carbohydrates", "Fats", "Sugar", "Type", "UnitId")
 VALUES
 	(42, 'Banane', 89, 1.1, 22.8, 0.3, 12.2, 1, 11),
 	(43, 'Narandža', 47, 0.9, 11.8, 0.1, 9.4, 1, 11),
-	(44, 'Pasulj šareni', 127, 8.9, 22.8, 0.5, 0.6, 1, 11),
-	(45, 'Pasulj bijeli', 143, 9.0, 25.0, 0.5, 0.6, 1, 11),
-	(46, 'Špinat', 23, 2.9, 3.6, 0.4, 0.4, 1, 11),
-	(47, 'Žuta paprika svježa', 27, 1.0, 6.0, 0.2, 2.4, 1, 11),
-	(48, 'Crvena paprika svježa', 31, 1.0, 6.0, 0.3, 2.4, 1, 11),
-	(49, 'Zelena salata', 15, 1.0, 2.9, 0.2, 0.8, 1, 11),
-	(50, 'Krastavac svjež', 16, 0.7, 3.6, 0.1, 1.7, 1, 11),
-	(51, 'Grožđe -bijelo', 69, 0.7, 18.1, 0.2, 16.2, 1, 11),
-	(52, 'Grožđe -crno', 69, 0.7, 18.1, 0.2, 16.2, 1, 11),
-	(53, 'Kruške', 39, 0.4, 10.0, 0.1, 9.8, 1, 11),
-	(54, 'Limun', 29, 1.1, 9.3, 0.3, 2.5, 1, 11),
-	(55, 'Šljiva', 46, 0.7, 11.4, 0.3, 9.9, 1, 11),
-	(56, 'Svježi peršun-list', 36, 3.0, 6.3, 0.8, 0.9, 1, 11),
-    (57, 'Jabuka kisela', 52, 0.3, 13.8, 0.2, 10.4, 1, 11);
+	(44, 'Pasulj bijeli', 143, 9.0, 25.0, 0.5, 0.6, 1, 11),
+	(45, 'Crvena paprika svježa', 31, 1.0, 6.0, 0.3, 2.4, 1, 11),
+	(46, 'Zelena salata', 15, 1.0, 2.9, 0.2, 0.8, 1, 11),
+	(47, 'Krastavac svjež', 16, 0.7, 3.6, 0.1, 1.7, 1, 11),
+	(48, 'Grožđe -bijelo', 69, 0.7, 18.1, 0.2, 16.2, 1, 11),
+	(49, 'Grožđe -crno', 69, 0.7, 18.1, 0.2, 16.2, 1, 11),
+	(50, 'Kruške', 39, 0.4, 10.0, 0.1, 9.8, 1, 11),
+	(51, 'Limun', 29, 1.1, 9.3, 0.3, 2.5, 1, 11),
+	(52, 'Šljiva', 46, 0.7, 11.4, 0.3, 9.9, 1, 11),
+	(53, 'Svježi peršun-list', 36, 3.0, 6.3, 0.8, 0.9, 1, 11),
+    (54, 'Jabuka kisela', 52, 0.3, 13.8, 0.2, 10.4, 1, 11),
+	(55, 'Hljeb bijeli sječeni', 265, 9, 49, 3.2, 5, 2, 11),
+	(56, 'Integralni hljeb', 250, 8, 42, 2.5, 3, 2, 11),
+	(57, 'Kukuruzni griz', 362, 7, 79, 3.8, 0.6, 2, 11),
+	(58, 'Pšenični griz', 352, 13, 73, 1, 0.5, 2, 11),
+	(59, 'Dvopek', 407, 12, 78, 5, 6, 2, 11),
+    (60, 'Pavlaka', 200, 2.5, 4, 18, 4, 3, 11),
+    (61, 'Jogurt', 100, 3.5, 4.7, 16, 4.7, 3, 12),
+    (62, 'Sir (sirko) za mazanje', 350, 8, 3, 35, 3, 3, 11),
+    (63, 'Topljeni sir', 300, 10, 2, 25, 2, 3, 11),
+    (64, 'Sir gauda', 356, 25, 2, 28, 2, 3, 11),
+    (65, 'Đuveč', 56, 2.6, 10.3, 0.7, 2.9, 1, 11),
+    (66, 'Kukuruz šećerac', 86, 3.22, 18.7, 1.35, 6.26, 1, 11),
+    (67, 'Ajvar blagi', 50, 1, 10, 1, 4, 5, 11),
+    (68, 'Mješana marmelada', 238, 0.3, 60.3, 0.1, 59.2, 1, 11),
+    (69, 'Kisela paprika file', 20, 0.9, 4.1, 0.1, 2.5, 1, 11),
+    (70, 'Pasterizovana cvekla', 43, 1.5, 10, 0.2, 7, 1, 11),
+	(71, 'Tunjevina komadići', 132, 30, 0, 0.7, 0, 4, 11),
+	(72, 'Riblja pašteta', 150, 20, 0, 6, 0, 5, 11),
+	(73, 'Rezanca za supu sa jajima', 360, 12, 60, 2, 0, 2, 11),
+	(74, 'Široka rezanca za juhu sa jajima', 350, 12, 65, 2, 0, 2, 11),
+	(75, 'Špagete bez jaja', 347, 13, 64, 2.5, 3.5, 2, 11),
+	(76, 'So', 0, 0, 0, 0, 0, 5, 11),
+	(77, 'Biber mljeveni', 251, 10, 64, 3.3, 0, 5, 11),
+	(78, 'Začin za jelo', 0, 0, 0, 0, 0, 5, 11),
+	(79, 'Šećer sitni', 400, 0, 100, 0, 100, 5, 11),
+	(80, 'Kafa mljevena', 2, 0.1, 0, 0, 0, 5, 11),
+	(81, 'Čaj nana', 0, 0, 0, 0, 0, 5, 12),
+	(82, 'Čaj šipak', 0, 0, 0, 0, 0, 5, 12),
+	(83, 'Čaj kamilica', 0, 0, 0, 0, 0, 5, 12),
+	(84, 'Uvin čaj', 0, 0, 0, 0, 0, 5, 12),
+	(85, 'Kokos brašno', 660, 7, 25, 65, 7, 5, 11),
+	(86, 'Kakao prah', 228, 19, 57, 14, 0, 5, 11),
+	(87, 'Vanilin šećer', 400, 0, 100, 0, 100, 5, 11),
+	(88, 'Puding vanila', 330, 0, 80, 0, 60, 5, 11),
+	(89, 'Puding čokolada', 340, 0, 80, 0, 65, 5, 11),
+	(90, 'Suvi kvasac', 325, 8, 68, 2, 0, 5, 11),
+	(91, 'Keks sa maslacem', 500, 6, 60, 28, 20, 2, 11),
+	(92, 'Suve šljive bez koštica', 240, 2, 64, 0.3, 38, 1, 11),
+	(93, 'Senf', 80, 5, 0, 5, 0, 5, 11),
+	(94, 'Majonez', 700, 1, 1, 75, 1, 5, 11),
+	(97, 'Mak', 525, 19, 28, 42, 0, 5, 11),
+	(98, 'Susam', 573, 17, 23, 50, 0, 5, 11),
+	(99, 'Žele bombone', 350, 0, 85, 0, 55, 5, 11),
+	(100, 'Grožđice', 299, 3, 79, 0, 59, 1, 11),
+	(101, 'Šećer u prahu', 400, 0, 100, 0, 100, 5, 11),
+	(102, 'Čokolada za kuvanje', 540, 7, 45, 31, 30, 5, 11),
+	(103, 'Pšenica bjelica', 340, 10, 70, 2, 0, 2, 11),
+	(104, 'Med', 304, 0.3, 82, 0, 82, 5, 11),
+	(105, 'Lavorov list', 5, 0.1, 1.2, 0, 0, 5, 11),
+	(106, 'Origano', 306, 10, 68, 6, 0, 5, 11),
+	(107, 'Sušeni peršun', 295, 9, 54, 1, 0, 5, 11),
+	(108, 'Sušeni list celera', 270, 8, 45, 2, 0, 5, 11),
+	(109, 'Kim', 333, 17, 52, 8, 0, 5, 11),
+	(110, 'Masline zelene bez košpica', 145, 1, 4, 15, 0, 1, 11),
+	(111, 'Šlag pjena', 375, 0, 43, 20, 43, 5, 11),
+	(112, 'Orah jezgro', 654, 15, 14, 65, 0, 1, 11),
+	(121, 'Soda bikarbona', 0, 0, 0, 0, 0, 5, 11),
+	(123, 'Supa iz kesice (kokošija)', 50, 5, 4, 2, 0, 5, 11),
+	(125, 'Oblandea', 400, 5, 60, 20, 20, 5, 11),
+	(126, 'Piskote', 400, 4, 65, 18, 20, 5, 11),
+	(127, 'Brašno bez glutena', 340, 5, 72, 1, 0, 5, 11),
+	(128, 'Tjestenina bez glutena', 360, 7, 72, 1.5, 0, 2, 11),
+	(129, 'Plazma keks', 400, 6, 60, 18, 20, 5, 11),
+	(130, 'Panirani kolutici lignje', 250, 15, 20, 12, 0, 4, 11),
+	(132, 'Panirani riblji štapići', 250, 15, 20, 12, 0, 4, 11);
 
 
-    //////////////////
-    INSERT INTO kitchen."Ingredients"(
-	"Id", "Name", "Calories", "Proteins", "Carbohydrates", "Fats", "Sugar", "Type", "UnitId")
-VALUES
-	(58, 'Jaja m', 143, 13.0, 1.1, 10.0, 1.1, 1, 11),
-	(59, 'Riba (oslić smrznut)', 85, 18.0, 0.0, 0.5, 0.0, 1, 11),
-	(60, 'Tunjevina komadići 150g-170g', 132, 29.0, 0.0, 1.0, 0.0, 1, 11),
-	(61, 'Goveđa kocka za supu 10/1', 250, 30.0, 0.0, 15.0, 0.0, 1, 11),
-	(62, 'Riblja pašteta od 150g-170g', 180, 10.0, 3.0, 14.0, 0.0, 1, 11),
-	(63, 'Rezanca za supu sa jajima', 350, 8.0, 70.0, 2.0, 1.0, 1, 11),
-	(64, 'Makaroni sa jajima', 360, 10.0, 70.0, 2.0, 1.0, 1, 11),
-	(65, 'Makaroni bez jaja', 350, 7.0, 70.0, 1.0, 1.0, 1, 11),
-	(66, 'Kore za pitu 500gr', 400, 7.0, 75.0, 10.0, 2.0, 1, 11),
-	(67, 'Široka rezanca za juhu sa jajima', 370, 8.0, 70.0, 3.0, 2.0, 1, 11),
-	(68, 'Špagete bez jaja 500g', 350, 7.0, 70.0, 1.0, 1.0, 1, 11),
-	(69, 'Pirinač dugo zrno 1kg', 365, 7.0, 80.0, 0.5, 0.1, 1, 11),
-	(70, 'So za jelo 1kg', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(71, 'Biber mljeveni 100g', 251, 8.0, 64.0, 3.3, 0.0, 1, 11),
-	(72, 'Paprika mljevena slatka 100g', 282, 12.0, 60.0, 1.0, 0.0, 1, 11),
-	(73, 'Začin za jelo 20%', 150, 5.0, 30.0, 1.0, 5.0, 1, 11),
-	(74, 'Šećer sitni 25kg', 400, 0.0, 100.0, 0.0, 100.0, 1, 11),
-	(75, 'Kafa mljevena', 2, 0.1, 0.0, 0.0, 0.0, 1, 11),
-	(76, 'Čaj nana 20g kesica', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(77, 'Čaj šipak 20g kesica', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(78, 'Čaj kamilica 20g kesica', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(79, 'Uvin čaj 20-30g kesica', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(80, 'Margarin za kuvanje 250g', 717, 0.0, 0.0, 80.0, 0.0, 1, 11),
-	(81, 'Ulje jestivo', 900, 0.0, 0.0, 100.0, 0.0, 1, 11),
-	(82, 'Sirće alkoholno 9%', 21, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(83, 'Kokos brašno 100g', 660, 6.0, 15.0, 64.0, 5.0, 1, 11),
-	(84, 'Kakao prah 100g', 228, 20.0, 58.0, 14.0, 0.0, 1, 11),
-	(85, 'Prašak za pecivo 10g', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(86, 'Vanilin šećer 10g', 40, 0.0, 10.0, 0.0, 10.0, 1, 11),
-	(87, 'Puding vanila 40g', 148, 1.2, 35.0, 0.0, 29.0, 1, 11),
-	(88, 'Puding čokolada 40g', 156, 1.4, 36.0, 0.0, 30.0, 1, 11),
-	(89, 'Suvi kvasac', 325, 9.0, 70.0, 1.0, 0.0, 1, 11),
-	(90, 'Keks sa maslacem', 520, 6.0, 70.0, 25.0, 20.0, 1, 11),
-	(91, 'Suve šljive bez koštica', 240, 2.0, 63.0, 0.2, 38.0, 1, 11),
-	(92, 'Senf pakovanje 1kg', 160, 8.0, 8.0, 8.0, 0.0, 1, 11),
-	(93, 'Majonez pakovanje 1kg', 700, 3.0, 1.0, 75.0, 0.0, 1, 11),
-	(94, 'Kečap blagi 1kg', 100, 1.0, 24.0, 0.2, 22.0, 1, 11),
-	(95, 'Šećer za kafu 0,05g', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(96, 'Mak', 525, 20.0, 28.0, 42.0, 0.0, 1, 11),
-	(97, 'Susam', 573, 18.0, 23.0, 50.0, 0.0, 1, 11),
-	(98, 'Žele bombone', 330, 0.0, 80.0, 0.0, 60.0, 1, 11),
-	(99, 'Grožđice', 299, 3.1, 79.0, 0.5, 66.0, 1, 11),
-	(100, 'Šećer u prahu', 400, 0.0, 100.0, 0.0, 100.0, 1, 11),
-	(101, 'Čokolada za kuvanje', 535, 6.0, 59.0, 32.0, 0.0, 1, 11),
-	(102, 'Pšenica bjelica', 340, 13.0, 72.0, 2.0, 0.0, 1, 11),
-	(103, 'Med', 304, 0.3, 82.4, 0.0, 82.4, 1, 11),
-	(104, 'Lovorov list', 313, 7.0, 74.0, 9.0, 0.0, 1, 11),
-	(105, 'Origano', 306, 9.0, 68.0, 6.0, 0.0, 1, 11),
-	(106, 'Sušeni peršun', 295, 11.0, 64.0, 1.0, 0.0, 1, 11),
-	(107, 'Sušeni list celera', 301, 11.0, 64.0, 1.0, 0.0, 1, 11),
-	(108, 'Kim', 333, 17.0, 54.0, 14.0, 0.0, 1, 11),
-	(109, 'Masline zelene bez košpica', 145, 0.6, 4.1, 15.0, 0.0, 1, 11),
-	(110, 'Šlag pjena', 340, 0.5, 10.0, 26.0, 21.0, 1, 11),
-	(111, 'Orah jezgro', 654, 15.0, 14.0, 65.0, 2.6, 1, 11),
-	(112, 'Bistri so jabuka tet.100% voće', 10, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(113, 'Sok narandža tet.100% voće', 45, 0.7, 11.0, 0.1, 8.0, 1, 11),
-	(114, 'Sok borovnica tet.50%voće', 57, 0.2, 13.0, 0.0, 11.0, 1, 11),
-	(115, 'Sok breskva tet.50%voće', 50, 0.3, 12.0, 0.0, 9.0, 1, 11),
-	(116, 'Gazirana voda 1,5l', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(117, 'Coca Cola 2l', 42, 0.0, 10.0, 0.0, 10.0, 1, 11),
-	(118, 'Fanta 2l', 44, 0.0, 11.0, 0.0, 11.0, 1, 11),
-	(119, 'Voda prirodna 0,33l', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(120, 'Soda bikarbona 1kg', 0, 0.0, 0.0, 0.0, 0.0, 1, 11),
-	(121, 'Sojine ljuspice 1kg', 400, 36.0, 30.0, 15.0, 0.0, 1, 11),
-	(122, 'Supa iz kesice (kokošija)', 35, 2.0, 6.0, 0.5, 1.0, 1, 11),
-	(123, 'Integralni pirinač 1kg', 370, 7.0, 77.0, 1.0, 0.5, 1, 11),
-	(124, 'Oblandea 200g', 400, 5.0, 70.0, 15.0, 35.0, 1, 11),
-	(125, 'Piskote od 400g do 420g', 400, 6.0, 80.0, 10.0, 20.0, 1, 11),
-	(126, 'Brašno bez glutena', 366, 7.0, 76.0, 1.0, 0.0, 1, 11),
-	(127, 'Tjestenina bez glutena 400g', 360, 7.0, 70.0, 2.0, 1.0, 1, 11),
-	(128, 'Plazma keks ili ekvivalent 300g', 540, 8.0, 70.0, 28.0, 25.0, 1, 11),
-	(129, 'Panirani kolutici lignje', 250, 8.0, 20.0, 12.0, 1.0, 1, 11),
-	(130, 'Mliječne pogačice za kafu', 550, 8.0, 70.0, 25.0, 20.0, 1, 11),
-	(131, 'Panirani riblji štapići', 250, 10.0, 15.0, 10.0, 1.0, 1, 11);
 
