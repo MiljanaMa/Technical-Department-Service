19bf612610d4503aa38e05d740498bc136bcbdc6
MiljanaMa <miljana.marjanovic9@gmail.com>
2024-08-03 14:07:16 +0200

    feat: add pdf export for menu

--- Technical-Department-front/src/app/kitchen/menus/menus.component.ts
+++ Technical-Department-front/src/app/kitchen/menus/menus.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
 import { KitchenService } from '../kitchen.service';
 import { WeeklyMenu, WeeklyMenuStatus, WeeklyMenuStatusLabels } from '../model/weekly-menu.model';
 import { DailyMenu, DayOfWeek, DayOfWeekLabels } from '../model/daily-menu.model';
 import { ConsumerType, ConsumerTypeLabels, MealOffer, MealType, MealTypeLabels } from '../model/meal-offer.model';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-new-menu',
@@ -11,6 +14,9 @@ import { ConsumerType, ConsumerTypeLabels, MealOffer, MealType, MealTypeLabels }
   styleUrls: ['./menus.component.css']
 })
 export class MenusComponent implements OnInit {
+
+  @ViewChild('menuStatusTabGroup') menuStatusTabGroup!: MatTabGroup;
+  @ViewChildren('dayTabGroups') dayTabGroups!: QueryList<MatTabGroup>;
 
   currentWeeklyMenu: WeeklyMenu | undefined;
   newWeeklyMenu: WeeklyMenu | undefined;
@@ -159,17 +165,13 @@ export class MenusComponent implements OnInit {
     if (!this.selectedWeeklyMenu?.from) return '';
 
     const fromDate = new Date(this.selectedWeeklyMenu.from);
-    fromDate.setDate(fromDate.getDate() + dayIndex);
-    return this.formatToDayMonthYear(fromDate);
+    const date = fromDate.toLocaleDateString('sr-RS', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+    return date;
   }
-
-  formatToDayMonthYear(date: Date): string {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear().toString().slice(-2);
-    return `${day}/${month}/${year}`;
-  }
-
 
   showTabularDefaultMenu(): void {
     const menus: DailyMenu[] = [];
@@ -216,6 +218,77 @@ export class MenusComponent implements OnInit {
 
   changeNewMenu(): void {
     this.router.navigate(['/tabular-menu/new']);
+  }
+  formatDateIntoSrb(date: Date): string {
+    return date.toLocaleDateString('sr-RS', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }
+
+  async exportToPDF() {
+    if (!this.menuStatusTabGroup || !this.dayTabGroups) {
+      console.error('Tab groups are not defined');
+      return;
+    }
+  
+    const pdf = new jsPDF();
+    pdf.setFontSize(10);
+    let position = 0;
+    const menuStatusTabs = this.menuStatusTabGroup._tabs.toArray();
+
+    //get monday
+    const today = new Date();
+    const day = today.getDay();
+    const diff = (day === 0 ? -6 : 1) - day;
+    const monday = new Date(today.setDate(today.getDate() + diff));
+    const i = 0;
+    
+  
+    for (let i = 0; i < menuStatusTabs.length; i++) {
+      this.menuStatusTabGroup.selectedIndex = i;
+      await this.sleep(500); // Wait for the content to render
+  
+      const dayTabGroup = this.dayTabGroups.find((_, index) => index === i);
+      if (dayTabGroup) {
+        const dayTabs = dayTabGroup._tabs.toArray();
+  
+        for (let j = 0; j < dayTabs.length; j++) {
+          dayTabGroup.selectedIndex = j;
+          await this.sleep(500); // Wait for the content to render
+
+          const tabContent = dayTabGroup._elementRef.nativeElement.closest('.tab-content');
+          const canvas = await html2canvas(tabContent as HTMLElement);
+          const imgData = canvas.toDataURL('image/png');
+          const imgWidth = 210; // A4 width in mm
+          const pageHeight = 295; // A4 height in mm
+          const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+          if (position + imgHeight > pageHeight) {
+            pdf.addPage(); // Add a new page
+            position = 0; // Reset position to the top of the new page
+          }
+          monday.setDate(monday.getDate() + i);
+          i = 1;
+          if (this.formatDate(monday)) {
+            pdf.text(`Datum: ${this.formatDateIntoSrb(monday)}`, 0, position + 10);
+            position += 20;
+          }
+          
+          pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+          position += imgHeight + 10;
+        }
+      }
+    }
+  
+    pdf.save('jelovnik.pdf');
+    this.getCurrentMenu();
+  }
+  
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
